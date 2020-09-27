f233fad481795df6d7edb9d6d2abed470e1d5c5b
Jun Han <formulahendry@gmail.com>
2020-09-27 13:39:24 +0800

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,3 @@
-az extension add --source https://modstestshared.blob.core.windows.net/tools/teamscloud-0.2.3-py2.py3-none-any.whl
-az extension add --source https://modstestshared.blob.core.windows.net/tools/teamsdev-0.0.2-py2.py3-none-any.whl
+az extension add --source https://modstestshared.blob.core.windows.net/tools/teamscloud-0.2.3-py2.py3-none-any.whl --yes
+az extension add --source https://modstestshared.blob.core.windows.net/tools/teamsdev-0.0.2-py2.py3-none-any.whl --yes
 wget -qO- https://aka.ms/install-artifacts-credprovider.sh | bash
