ef55e5bf505cfb9cc1e2bde3e043a0c2410c69b4
Jun Han <formulahendry@gmail.com>
2020-09-27 13:30:28 +0800

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,3 @@
-curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 az extension add --source https://modstestshared.blob.core.windows.net/tools/teamscloud-0.2.3-py2.py3-none-any.whl
 az extension add --source https://modstestshared.blob.core.windows.net/tools/teamsdev-0.0.2-py2.py3-none-any.whl
 wget -qO- https://aka.ms/install-artifacts-credprovider.sh | bash
